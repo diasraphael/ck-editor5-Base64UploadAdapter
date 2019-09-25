16d8085dd5af1c09f8edbe58886d87ffc7fa59e1
Dias Raphael <diasraphael88@gmail.com>
2019-09-25 05:56:35 +0200

    Added changes for new setup

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -11,10 +11,6 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
-import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
-import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -24,7 +20,6 @@ import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
@@ -32,14 +27,6 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-
-import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/base64uploadadapter';
-import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment'; 
-import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
-import Font from '@ckeditor/ckeditor5-font/src/font';
-
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -61,20 +48,11 @@ ClassicEditor.builtinPlugins = [
 	ImageUpload,
 	Link,
 	List,
+	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar,
-	Base64UploadAdapter,
-	Alignment,
-	Highlight,
-	Underline,
-	Strikethrough,
-	Subscript,
-	Superscript,
-	Indent,
-	IndentBlock,
-	Font
+	TableToolbar
 ];
 
 // Editor configuration.
@@ -83,53 +61,26 @@ ClassicEditor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
-			'fontSize',
-			'fontFamily',
-			'fontColor',
-			'fontBackgroundColor',
-			'|',
 			'bold',
 			'italic',
-			'underline',
-			'strikethrough',
-			'blockQuote',
-			'subscript',
-			'superscript',
-			'|',
-			'highlight',
-			'outdent',
-			'indent',
-			'alignment',
-			'bulletedList', 
-			'numberedList', 
-			'|',
 			'link',
+			'bulletedList',
+			'numberedList',
 			'imageUpload',
+			'blockQuote',
 			'insertTable',
+			'mediaEmbed',
 			'undo',
 			'redo'
 		]
 	},
-	plugins :[ Image, ImageToolbar, ImageCaption, ImageStyle, ImageResize]
-	,
 	image: {
 		toolbar: [
-			'imageTextAlternative',
-			'|', 
-			'imageStyle:alignLeft', 
-			'imageStyle:full', 
-			'imageStyle:alignRight' 
-		],
-		styles: [
-                // This option is equal to a situation where no style is applied.
-                'full',
-
-                // This represents an image aligned to the left.
-                'alignLeft',
-
-                // This represents an image aligned to the right.
-                'alignRight'
-        ]
+			'imageStyle:full',
+			'imageStyle:side',
+			'|',
+			'imageTextAlternative'
+		]
 	},
 	table: {
 		contentToolbar: [
