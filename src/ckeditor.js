ab32162bac7883ab61be4ef8dac584346c87e19f
Dias Raphael <diasraphael88@gmail.com>
2019-09-25 07:01:14 +0200

    Added changes for new setup

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -24,6 +24,7 @@ import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
@@ -106,6 +107,7 @@ ClassicEditor.defaultConfig = {
 			'redo'
 		]
 	},
+	plugins: [ Image, ImageToolbar, ImageCaption, ImageStyle, ImageResize ],
 	image: {
 		toolbar: [
 			'imageStyle:full',
